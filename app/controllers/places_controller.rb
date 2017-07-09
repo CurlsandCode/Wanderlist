b939fe194d063446a07718685508e748dd2967cb
Monika <monika.m.williams@gmail.com>
2017-07-09 21:33:02 +0900

    made changes to the edit, delete, and patch routes, and mage changes to the edit views

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,17 +21,24 @@ class PlacesController < ApplicationController
     
     get '/lists/:list_id/places/:places_id/edit' do #loads edit form
         @list = List.find(params[:list_id])
-        @place = @lists.places.find(params[:place_id])
+        @place = Place.find_by(params[:id])
          erb :"places/edit"
     end
     
 
-    patch '/lists/:list_id/places/:place_id' do #edits a place
-    @place.update_attributes(params[:place])
+    patch '/lists/:list_id/places/:id' do #edits a place
+      @list = List.find(params[:list_id])
+      @place = Place.find_by(params[:id])
+      @place.name = params[:name]
+      @place.content = params[:content]
+      @place.list_id = params[:list]
+      @place.save
     redirect "/lists/#{params[:list_id]}/places"
     end
 
    delete '/lists/:list_id/places/:place_id' do #deletes place
+    @list = List.find(params[:list_id])
+    @place = Place.find_by(params[:id])
     @place.destroy
     redirect "/lists/#{params[:list_id]}/places"
     end
