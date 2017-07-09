c0e3086f8848df6dd4a0960b44fd954ff15fb29d
Monika <monika.m.williams@gmail.com>
2017-07-09 14:50:00 +0900

    add if statements to routes to check if a user is logged in and added a rout for logging out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,43 @@
 class UsersController < ApplicationController
-    
-get "/" do
-    erb :index
-  end
+
     
     get '/login' do
-       
+      if logged_in?
+    redirect to '/lists'
+     else
     erb :'/users/login'
+    end
     end
     
     post '/login' do
-    user= User.find(username: params[:username])
-        redirect '/lists'
+    @user = User.find(username: params[:username])
+     if @user && user.authenticate(params[:password])
+    session[:id] = @user.id
+     redirect '/lists'
+    else
+     redirect to '/login'    
     end
     
     get '/signup' do
+     if !logged_in?
     erb :'/users/create'
+     else
+     redirect to '/lists'
+    end
     end
     
     post '/signup' do
-        user = User.new(params)
-        user.save
+    redirect to '/' if logged_in?
+    @user = User.new(params)
+    if @user.save
         redirect '/lists'
+     else
+        redirect to '/users/create'
+    end
+    end
+    
+    get '/logout' do
+    session.clear
+    redirect to '/login'
     end
 end
