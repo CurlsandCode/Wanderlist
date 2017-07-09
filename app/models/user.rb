f31b2730593e6c36b8645d6f7c7aefce563b455b
Monika <monika.m.williams@gmail.com>
2017-07-09 14:56:25 +0900

    added validations and associations to user model and added associations to place and list models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_many :lists
+    has_many :places, through: :lists
+    validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true
     has_secure_password
 end
