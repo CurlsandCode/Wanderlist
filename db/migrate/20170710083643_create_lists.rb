78839c810e08c895665f7371348c26ce01d54c26
Mo <monika.m.williams@gmail.com>
2017-07-10 22:58:47 +0900

    delete
    deleted in local repository

--- db/migrate/20170710083643_create_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateLists < ActiveRecord::Migration
-  def change
-       create_table :lists do |t|
-          t.string :name
-          t.integer :user_id
-          t.integer :place_id
-      end
-  end
-end
