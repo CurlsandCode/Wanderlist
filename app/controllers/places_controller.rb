12a31904e0be34b1c630cb2b6d8c2a48c70c02e2
Monika <monika.m.williams@gmail.com>
2017-07-13 21:38:07 +0900

    made small changes to user places view and updated places controller removing lists

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,27 +1,33 @@
 class PlacesController < ApplicationController
     
-    get '/lists/:list_id/places' do #loads all places
-        @list = List.find_by_id(params[:list_id])
-        @place = Place.find_by(params[:place_id])
+    get '/places' do
+        if logged_in?#loads all places
         @places = Place.all
         erb :"/places/places"
+       else
+       redirect to '/login'
+      end
     end
     
-    get '/lists/:list_id/places/new' do #loads new form
-        @list = List.find(params[:list_id])
+    get '/places/new' do #loads new form
+     if logged_in?
+     erb :'/places/new'
+     else
+     redirect to "/login"
+     end
+    end
+    
+    post '/places' do #creates a place
+      place = current_user.places.build(params[:place])
+       if place.save
+      redirect to "/places"
+       else
         erb :'/places/new'
-    end
-    
-    post '/lists/:list_id/places' do #creates a place
-        @list = List.find(params[:list_id])
-        @place = Place.create(params[:place])
-        @place.save
-      redirect "/lists/#{params[:list_id]}/places"
+       end
      end
     
-    get '/lists/:list_id/places/:places_id/edit' do #loads edit form
+    get '/places/:places_id/edit' do #loads edit form
         if logged_in?
-        @list = List.find_by(params[:id])
         @place = current_user.places.find_by(id: params[:id])
         if @place
          erb :"places/edit"
@@ -30,26 +36,24 @@ class PlacesController < ApplicationController
       end
     end
  end
-    patch '/lists/:list_id/places/:place_id' do #edits a place
+    patch '/places/:place_id' do #edits a place
     if logged_in?
-      @list = List.find_by(params[:id])
       place = current_user.places.find_by_id(id: params[:id])
       place.update(name: params[:name], content: params[:content])
-      redirect "/lists/#{params[:list_id]}/places"
+      redirect "/places"
         else
         redirect '/'
       end
     end
 
-   delete '/lists/:list_id/places/:place_id' do #deletes place
+   get '/places/:place_id/delete' do #deletes place
     if logged_in?
-    @list = List.find_by(params[:id])
     place = current_user.places.find_by(id: params[:id])
     if place
     place.delete
-    redirect "/lists/#{params[:list_id]}/places"
+    redirect "/places"
     else
-        redirect '/lists'
+        redirect '/places/3{params[:id]}'
      end
     end
    end
