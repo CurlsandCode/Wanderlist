6cc53372966720c1766a46d2fb17c21e7c64c910
Monika <monika.m.williams@gmail.com>
2017-07-26 18:25:57 +0900

    made changes to files in an attempt to get heroku working

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,18 @@
+require 'bundler'
+Bundler.require(:default, :development, :production)
 ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
   ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
 end
 
+
 configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
+  db =  URI.parse(ENV['DATABASE_URL'] || 'postgres_database_url')
+#  db =  'postgres://localhost/topofthemorning_development'
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
@@ -19,8 +20,11 @@ configure :production do
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
-    :encoding => 'utf8'
+    :encoding => 'UTF8'
+
   )
 end
 
+require_relative '../app/controllers/application_controller.rb'
 require_all 'app'
+set :public_folder, File.dirname(__FILE__) + '/public'
